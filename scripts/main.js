fbb502fab3c564a548c99a125fb76f0a38a820a6
bryan-onwas <bryan.onwas@gmail.com>
2018-11-21 11:22:58 -0600

    Added hamburger icon

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,15 @@
 var header = document.querySelector('header');
 var sideBar = document.querySelector('aside');
 
+  // Look for .hamburger
+  var hamburger = document.querySelector(".hamburger");
+  // On click
+  hamburger.addEventListener("click", function() {
+    // Toggle class "is-active"
+    hamburger.classList.toggle("is-active");
+    // Do something else, like open/close menu
+  });
+
 header.addEventListener('click', function() {
-  sideBar.classList.toggle('sidebar__isOpen');
+  sideBar.classList.toggle('sidebar__open');
 });
