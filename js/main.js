a59fb14bad92ac26e3d922b11fe846680a670cdf
bryan-onwas <bryan.onwas@gmail.com>
2018-12-30 23:41:16 -0600

    Website is now SASS Powered

--- js/main.js
+++ js/main.js
@@ -4,3 +4,25 @@ var modal = document.querySelector('.container');
 var bar = document.querySelector('body');
 var test = document.querySelector('.test');
 var head = document.querySelector('header');
+
+hamburger.addEventListener('click', iconClick);
+window.addEventListener('click', outsideClick);
+
+// Hamburger icon click functionally
+function iconClick() {
+  hamburger.classList.toggle('is-active');
+  sideBar.classList.toggle('sidebar__open');
+  modal.classList.toggle('container__active');
+  bar.classList.toggle('body__active');
+}
+
+// Outside click functionally (in case of modal/sidebar outside clicks)
+// Listen for outside clicks
+function outsideClick(event) {
+  if (event.target.id === 'modal--space') {
+    hamburger.classList.remove('is-active');
+    sideBar.classList.remove('sidebar__open');
+    modal.classList.remove('container__active');
+    bar.classList.remove('body__active');
+  }
+}
